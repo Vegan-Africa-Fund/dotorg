6fd03326f8e8e1a80b4e4de031d11b1a3979033b
blackpintz <rosewanjohi24@gmail.com>
2021-03-09 12:08:50 +0300

    finished styling for the header menu.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,28 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { withStyles } from '@material-ui/core/styles';
 import {Button} from '@material-ui/core'
-import styles from '../styles/Layout.module.css'
+import { green } from '@material-ui/core/colors';
 
 type Props = {
   children?: ReactNode
   title?: string
 }
+
+const BootstrapButton = withStyles({
+  root: {
+    margin: "0.6rem 0.3rem 0 0.3rem",
+    '&:hover': {
+      backgroundColor: green[500],
+      color: "#fff"
+    },
+    '&:focus': {
+      border: '2px solid',
+      borderColor: "#fff"
+    },
+  },
+})(Button);
 
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
@@ -18,15 +33,15 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
-      <nav className={styles.container}>
+      <nav>
         <Link href="/" passHref>
-          <button className = {styles.btn}>Home</button>
+          <BootstrapButton variant = "outlined" color="primary">Home</BootstrapButton>
         </Link>
         <Link href="/give" passHref>
-          <Button variant = "outlined" color="primary">Give</Button>
+          <BootstrapButton variant = "outlined" color="primary">Give</BootstrapButton>
         </Link>
         <Link href="/info" passHref>
-          <button>Info</button>
+          <BootstrapButton variant = "outlined" color="primary">Info</BootstrapButton>
         </Link>
       </nav>
     </header>
