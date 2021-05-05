7b381868dc184a9f92d24710adaf117fed250aa9
blackpintz <rosewanjohi24@gmail.com>
2021-05-05 17:34:27 +0300

    add a notification bar after successful sign up.

--- components/Email.tsx
+++ components/Email.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
 import {API, graphqlOperation} from 'aws-amplify'
 import Amplify from 'aws-amplify'
-import {TextField, Button, Box} from "@material-ui/core"
+import {TextField, Button, Box, Snackbar} from "@material-ui/core"
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
 import amplifyConfig from '../aws-exports'
 
@@ -33,6 +34,15 @@ mutation createVegan($email: String!) {
 const Email = () => {
     const classes = useStyles();
     const [email, setEmail]=useState('')
+    const [open, setOpen] = useState(false)
+
+    const Alert = (props: AlertProps) => {
+        return <MuiAlert elevation={6} variant="filled" {...props} />;
+      }
+
+    const handleClose = () => {
+        setOpen(false)
+      }
 
     const emailData = {
         email
@@ -43,26 +53,35 @@ const Email = () => {
         try {
             await API.graphql(graphqlOperation(createVegan, emailData))
             setEmail('')
+            setOpen(true)
         } catch (err) {
             console.log(err)
         }
     }
     return (
-    <form onSubmit={handleSubmit}>
-        <TextField
-        placeholder="Enter Your Email"
-        fullWidth
-        required
-        variant="outlined"
-        type="email"
-        value={email}
-        onChange={(e) =>{setEmail(e.target.value)}}
-        className={classes.input}
-        />
-        <Box display="flex" justifyContent="center">
-            <Button className={classes.button} size="large" type="submit">SIGN UP</Button>
-        </Box>
-    </form>
+    <>
+        <form onSubmit={handleSubmit}>
+            <TextField
+            placeholder="Enter Your Email"
+            fullWidth
+            required
+            variant="outlined"
+            type="email"
+            value={email}
+            onChange={(e) =>{setEmail(e.target.value)}}
+            className={classes.input}
+            />
+            <Box display="flex" justifyContent="center">
+                <Button className={classes.button} size="large" type="submit">SIGN UP</Button>
+            </Box>
+        </form>
+        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success">
+            Sign-up successful!
+            </Alert>
+        </Snackbar>
+
+    </>
 )}
 
 export default Email
