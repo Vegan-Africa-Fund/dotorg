089c17a9ef255daf5b85db8ababda8419cdda2bf
blackpintz <rosewanjohi24@gmail.com>
2021-05-12 16:27:05 +0300

    made statement section mobile responsive.

--- components/Statement.tsx
+++ components/Statement.tsx
@@ -13,11 +13,27 @@ const useStyles = makeStyles((theme) => ({
         paddingTop: "0.6rem",
         marginLeft: "0.8rem",
         fontFamily: "FuturaLight",
-        fontSize: "1.1rem"
+        fontSize: "1.1rem",
+        [theme.breakpoints.down('sm')]: {
+            width: "100%"
+        }
     },
 
     name: {
         fontStyle: "italic"
+    },
+    container: {
+        display: "flex",
+        [theme.breakpoints.down('sm')]: {
+            display: "block"
+        }
+    },
+    avatar: {
+        [theme.breakpoints.down('sm')]: {
+            display: "flex",
+            width: "100%",
+            justifyContent: "center"
+        }
     }
   }));
 
@@ -26,8 +42,10 @@ const Statement = () => {
     const classes = useStyles();
 
     return (
-        <Box display="flex">
-            <Avatar alt="Sunny" src="/images/Sunny.webp" className={classes.large} />
+        <Box className={classes.container}>
+            <Box className={classes.avatar}>
+                <Avatar alt="Sunny" src="/images/Sunny.webp" className={classes.large} />
+            </Box>
                 <Typography className={classes.paragraph} variant="body1">
                 “Veganism, historically, has been mostly used as a reactive technique by concerned individuals doing our part to improve health, act ethically, and reduce environmental impact. Vegan AF is ushering in a new era of utilizing veganism as a preventative approach to some of the world’s greatest challenges.” <span className={classes.name}>- Sunny, Vegan AF Founder</span> 
                 </Typography>
