b2fb93b932b2b78c355b29144a406409039c224e
blackpintz <rosewanjohi24@gmail.com>
2021-05-14 13:49:45 +0300

    fixed the read more button.

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import {Box, Typography, Divider, Grid} from '@material-ui/core';
-import SimpleAccordion from '../components/Accordion'
+import {Box, Typography, Divider, Grid, Button} from '@material-ui/core';
+import { deepPurple } from '@material-ui/core/colors';
 import Layout from '../components/Layout';
 
 const useStyles = makeStyles(theme => ({
@@ -67,10 +67,29 @@ const useStyles = makeStyles(theme => ({
         fontFamily: "MoonTime",
         fontSize: "2.6rem",
     },
+    showMore: {
+        color: deepPurple[500],
+      },
+
+    showLess: {
+        paddingLeft: "0",
+        color: deepPurple[500]
+    }
 }))
 
 const Team = () => {
     const classes = useStyles();
+    const[show, setShow] = useState(false)
+
+    console.log(show)
+
+    const handleMore = () => {
+        setShow(true)
+    }
+
+    const handleLess = () => {
+        setShow(false)
+    }
     return (
         <Layout title="Team | VeganAfrica">
         <Grid className={classes.grid} container>
@@ -92,8 +111,17 @@ const Team = () => {
                 <Typography variant="h5">Sunny Satva</Typography>
                 <Typography variant="h6">Founder & CEO</Typography>
                 <Typography className={classes.statement} variant="body1">
-                I started Vegan AF while traveling around East Africa, hoping to make a positive impact in all of the places I went. I love food and love to eat, and one of my favorite ways to impactfully spend my money is by supporting businesses that are aligned with my ethics and vision for the future.  While in Kenya, I realized that I don’t just want to buy from plant-based businesses - I want to help them scale their operations!  
-                <SimpleAccordion />
+                I started Vegan AF while traveling around East Africa, hoping to make a positive impact in all of the places I went. I love food and love to eat, and one of my favorite ways to impactfully spend my money is by supporting businesses that are aligned with my ethics and vision for the future.  While in Kenya, I realized that I don’t just want to buy from plant-based businesses - I want to help them scale their operations! 
+                {show ? (
+                    <>
+                    <span> I began personally impact investing in early 2021 and feel an incredible sense of fulfillment from supporting initiatives that are changing the world and dietary habits on the fastest-growing continent on earth. I believe part of my mission on earth is to redistribute wealth and help people see the vision I have for the future - a kinder, plant-based planet that will allow for beautiful human evolution for decades to come. I previously worked as an outreach director for the New York Public Interest Research Group (NYPIRG) and the Andrew Yang 2020 presidential campaign. I’m the author and publisher of Sunny Payne’s Soy-Free Vegan Cookbook (available on Amazon).</span>
+                    <Box>
+                        <Button onClick={handleLess} className={classes.showLess}>Show Less</Button>
+                    </Box>
+                    </>
+                ) : (
+                    <Button onClick={handleMore} className={classes.showMore}>Read More</Button>
+                )}
                 </Typography>
         </Box>
         </Box>
