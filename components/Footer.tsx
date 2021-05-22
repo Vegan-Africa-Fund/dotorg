4687df69a0459d8c134fb13f8ef413bd7910db08
blackpintz <rosewanjohi24@gmail.com>
2021-05-22 18:00:06 +0300

    remove box element in footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import {AppBar, Typography, Box, List, ListItem, Grid} from '@material-ui/core';
+import {AppBar, Typography, List, ListItem, Grid} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import SimpleDialogDemo from './Dialog'
 
