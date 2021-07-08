44ccdb8e491c073b5657423145046de4d4e6f59e
blobjx <80590794+blobjx@users.noreply.github.com>
2021-07-08 17:56:12 +0300

    removing some keys 🔐

--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -2,14 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const config = {
-  apiKey: "AIzaSyDRN1qwil5t5TOp5eod5nlg4dKyv6mWxX8",
-  authDomain: "crrntx.firebaseapp.com",
-  databaseURL: "https://crrntx-default-rtdb.firebaseio.com",
-  projectId: "crrntx",
-  storageBucket: "crrntx.appspot.com",
-  messagingSenderId: "663316229088",
-  appId: "1:663316229088:web:8c3d7bff87ffd306abd985",
-  measurementId: "G-K1HZ9G2PV0"
+  process.env.FIREBASE_CONFIG_JSON
   };
 
   if (!firebase.apps.length) {
